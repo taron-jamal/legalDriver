95655505d26aad632f99dc8440b7dc1c5d1f490c
taron-jamal <61537513+taron-jamal@users.noreply.github.com>
2020-03-14 01:30:11 -0700

    Update script.js
    Fix logical operator.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Prompt user for age and determine whether their legally suitable to drive.
-var legalDriver;
+var legalDriver = 18;
 prompt("Please enter your age:");
-  if (legalDriver === 18) {
+  if (legalDriver >= 18) {
     alert("You are legally able to drive this vehicle. Powering on.");
   } else (legalDriver < 18)
     alert("Sorry, you are too young to drive this vehicle. Powering off now.");
